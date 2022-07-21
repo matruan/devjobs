3576d963f54dec1990570e6699e293e6c017f3ac
Matias Ruiz <ruizmat@gmail.com>
2022-07-21 09:37:27 +0200

    Create add_columns_to_vacantes  migration to add necessary fields in our model

---
+++ database/migrations/2022_07_21_070653_add_columns_to_vacantes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vacantes', function (Blueprint $table) {
+            $table->string('titulo');
+            $table->foreignId('salario_id')->constrained()->onDelete('cascade');
+            $table->foreignId('categoria_id')->constrained()->onDelete('cascade');
+            $table->string('empresa');
+            $table->date('ultimo_dia');
+            $table->text('descripcion');
+            $table->string('imagen');
+            $table->integer('publicado')->default(1);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // We have to execute the relevant actions in each migration and not others. We cannot drop 
+        // the entire table because the table is generated in the first migration (create_vacantes_table)
+        Schema::table('vacantes', function (Blueprint $table) {
+            $table->dropColumn(['titulo', 'salario_id', 'categoria_id', 'empresa', 'ultimo_dia', 'descripcion', 'imagen', 'publicado', 'user_id']);
+        });
+    }
+};
