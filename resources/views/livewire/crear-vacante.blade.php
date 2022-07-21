60d5c89ff275b05d56cbcfd63d81d418262081e8
Matias Ruiz <ruizmat@gmail.com>
2022-07-21 08:52:21 +0200

    Showing image preview in crear-vacante view before creating a new vacant

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -112,6 +112,14 @@
           wire:model="imagen"
           accept="image/*"
         />
+
+        <div class="my-5 w-80">
+          @if($imagen)
+            Imagen:
+            <img src="{{ $imagen->temporaryUrl() }}">
+          @endif
+        </div>
+
         @error('imagen')
           <livewire:mostrar-alerta :message="$message" />
         @enderror
