cc968a9715042ab708da787d63ca11dffdaed97d
Matias Ruiz <ruizmat@gmail.com>
2022-07-23 20:12:11 +0200

    Using mount hook in EditarVancante livewire component to show values of each vacant attribute

--- app/Http/Livewire/EditarVacante.php
+++ app/Http/Livewire/EditarVacante.php
@@ -4,10 +4,18 @@ namespace App\Http\Livewire;
 
 use App\Models\Categoria;
 use App\Models\Salario;
+use App\Models\Vacante;
 use Livewire\Component;
 
 class EditarVacante extends Component
 {
+    public $titulo;
+
+    public function mount(Vacante $vacante)
+    {
+        $this->titulo = $vacante->titulo;
+    }
+
     public function render()
     {
         // Querying the database
