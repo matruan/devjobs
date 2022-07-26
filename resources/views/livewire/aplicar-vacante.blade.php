8f3ff8ed2c3f46069fad7e8bc49df78f4c128810
Matias Ruiz <ruizmat@gmail.com>
2022-07-26 10:15:39 +0200

    Adding form to aplicar-vacante livewire view component to upload CVs

--- resources/views/livewire/aplicar-vacante.blade.php
+++ resources/views/livewire/aplicar-vacante.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <p>Aquí se mostrará el formulario</p>
+<div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
+  <h3 class="text-center text-2xl font-bold my-4">Aplicar a esta vacante</h3>
+
+  <form class="w-96 mt-5" action="">
+    <div class="mb-4">
+        <x-label for="cv" :value="__('Curriculum')" />
+        <x-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full" />
+    </div>
+  </form>
 </div>
