d89b47252c76b7ed0361b8118452ec875ca7c8d4
Matias Ruiz <ruizmat@gmail.com>
2022-07-26 20:33:16 +0200

    Adding notifications number icon in the responsive navigation layout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -30,7 +30,8 @@
 
           @if (auth()->user()->rol === 2)
             <a class="mr-2 w-7 h-7 bg-indigo-600 hover:bg-indigo-800 rounded-full 
-              flex flex-col justify-center items-center text-sm font-extrabold text-white" href="{{ route('notificaciones') }}">
+              flex flex-col justify-center items-center text-sm font-extrabold text-white"
+              href="{{ route('notificaciones') }}">
               {{ Auth::user()->unreadNotifications->count() }}
             </a>
           @endif
@@ -103,6 +104,20 @@
         <x-responsive-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
           {{ __('Crear Vacante') }}
         </x-responsive-nav-link>
+
+        @if (auth()->user()->rol === 2)
+          <div class="flex gap-2 items-center p-3">
+            <a class="w-7 h-7 bg-indigo-600 hover:bg-indigo-800 rounded-full 
+              flex flex-col justify-center items-center text-sm font-extrabold text-white"
+              href="{{ route('notificaciones') }}">
+              {{ Auth::user()->unreadNotifications->count() }}
+            </a>
+            <p class="text-base font-medium text-gray-600">
+              @choice('Notificacion|Notificaciones', Auth::user()->unreadNotifications->count())
+            </p>
+          </div>
+        @endif
+
       </div>
 
       <!-- Responsive Settings Options -->
