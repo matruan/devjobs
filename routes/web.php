df9154e9f02def32dcddfc2e6896442cba5445e7
Matias Ruiz <ruizmat@gmail.com>
2022-07-26 22:19:58 +0200

    Implementing of the index method in CandidatoController to return the candidatos.index view from which we will iterate the candidates of a vacant

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CandidatoController;
 use App\Http\Controllers\VacanteController;
 use App\Http\Controllers\NotificacionController;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,7 @@ Route::get('/dashboard', [VacanteController::class,'index'])->middleware(['auth'
 Route::get('/vacantes/create', [VacanteController::class,'create'])->middleware(['auth', 'verified'])->name('vacantes.create');
 Route::get('/vacantes/{vacante}/edit', [VacanteController::class, 'edit'])->middleware(['auth', 'verified'])->name('vacantes.edit');
 Route::get('/vacantes/{vacante}', [VacanteController::class, 'show'])->name('vacantes.show');
+Route::get('/candidatos/{vacante}', [CandidatoController::class, 'index'])->name('candidatos.index');
 
 // Notifications
 Route::get('/notificaciones', NotificacionController::class)->middleware(['auth', 'verified', 'rol.reclutador'])->name('notificaciones');
