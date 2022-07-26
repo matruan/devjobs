5bddd98d2c0881f42560ca91892b909aa88cb43d
Matias Ruiz <ruizmat@gmail.com>
2022-07-26 11:07:50 +0200

    Linking / wiring aplicar-vacante livewire view with AplicarVacante livewire component

--- app/Http/Livewire/AplicarVacante.php
+++ app/Http/Livewire/AplicarVacante.php
@@ -6,6 +6,25 @@ use Livewire\Component;
 
 class AplicarVacante extends Component
 {
+    public $cv;
+
+    protected $rules = [
+        'cv' => 'required|mimes:pdf'
+    ];
+
+    public function aplicar()
+    {
+        $this->validate();
+
+        // save cv in local storage
+
+        // create applicant
+
+        // create notification and send email
+
+        // show message to the user saying everything is ok
+    }
+
     public function render()
     {
         return view('livewire.aplicar-vacante');
--- resources/views/livewire/aplicar-vacante.blade.php
+++ resources/views/livewire/aplicar-vacante.blade.php
@@ -1,11 +1,15 @@
 <div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
   <h3 class="text-center text-2xl font-bold my-4">Aplicar a esta vacante</h3>
 
-  <form class="w-96 mt-5" action="">
+  <form wire:submit.prevent="aplicar" class="w-96 mt-5">
     <div class="mb-4">
         <x-label for="cv" :value="__('Curriculum')" />
-        <x-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full" />
+        <x-input id="cv" type="file" wire:model="cv" accept=".pdf" class="block mt-1 w-full" />
     </div>
+    @error('cv')
+        <livewire:mostrar-alerta :message="$message" />
+        
+    @enderror
 
     <x-button class="my-5">
       {{ __('Aplicar') }}    
